6e6d5edc3f8a2f47182c2f59b8fa6ff8aaf3aea6
voidful <voidful.stack@gmail.com>
2021-11-08 22:43:03 +0800

    specific lna and lnae and fix train bug on bart model

--- example/train.py
+++ example/train.py
@@ -6,7 +6,7 @@ import asrp
 import speechmix
 from datasets import load_dataset, Audio
 import torch
-from transformers import Wav2Vec2Processor, Trainer, TrainingArguments
+from transformers import Wav2Vec2Processor, Trainer, TrainingArguments, EarlyStoppingCallback
 from typing import Dict, List, Union, Optional
 from dataclasses import dataclass
 
@@ -15,23 +15,23 @@ def main(arg=None):
     def prepare_dataset(batch):
         audio = batch["audio"]
         batch["input_values"] = model.processor(audio["array"], sampling_rate=16_000).input_values[0]
-        batch["labels"] = model.tokenizer(batch["sentence"]).input_ids
+        sent = batch["text"] if 'text' in batch else batch["sentence"]
+        batch["labels"] = model.tokenizer(sent).input_ids[1:]
         batch["input_ids"] = batch["labels"]
         return batch
 
     def compute_metrics(pred):
         pred_ids = pred.predictions
         pred_ids = [i[i != -100] for i in pred_ids]
-
-        pred_str = model.tokenizer.batch_decode(pred_ids)
+        pred_str = model.tokenizer.batch_decode(pred_ids, skip_special_tokens=True, group_tokens=False)
         # we do not want to group tokens when computing the metrics
         label_ids = pred.label_ids
         label_ids = [i[i != -100] for i in label_ids]
-        label_str = model.tokenizer.batch_decode(label_ids, group_tokens=False)
+        label_str = model.tokenizer.batch_decode(label_ids, skip_special_tokens=True, group_tokens=False)
 
         cer = asrp.cer(label_str, pred_str)
-
-        return {"cer": cer}
+        wer = asrp.wer(label_str, pred_str)
+        return {"cer": cer, "wer": wer}
 
     @dataclass
     class DataCollatorWithPadding:
@@ -47,7 +47,8 @@ def main(arg=None):
             # split inputs and labels since they have to be of different lenghts and need
             # different padding methods
             input_features = [{"input_values": feature["input_values"]} for feature in features]
-            label_features = [{"input_ids": feature["labels"]} for feature in features]
+            target_name = 'labels' if "labels" in features[0] else "input_ids"
+            label_features = [{"input_ids": feature[target_name]} for feature in features]
 
             batch = self.processor.pad(
                 input_features,
@@ -63,12 +64,9 @@ def main(arg=None):
                 max_length=self.max_length_labels,
                 pad_to_multiple_of=self.pad_to_multiple_of_labels,
                 return_tensors="pt",
-            )
+            ).input_ids
 
-            # replace padding with -100 to ignore loss correctly
-            labels = labels_batch["input_ids"].masked_fill(labels_batch.attention_mask.ne(1), -100)
-
-            batch["labels"] = labels
+            batch[target_name] = labels_batch
             batch.pop('attention_mask', None)
             return batch
 
@@ -79,8 +77,12 @@ def main(arg=None):
         parser.add_argument("--SpeechMixEED", action='store_true')
         parser.add_argument("--SpeechMixED", action='store_true')
         parser.add_argument("--SpeechMixSelf", action='store_true')
+        parser.add_argument("--dataset", type=str)
+        parser.add_argument("--field", type=str)
+        parser.add_argument("--split", type=str)
         parser.add_argument("--ftl", action='store_true')
         parser.add_argument("--lna", action='store_true')
+        parser.add_argument("--lnae", action='store_true')
         parser.add_argument("--fne", action='store_true')
 
         input_arg, model_arg = parser.parse_known_args(args)
@@ -89,16 +91,18 @@ def main(arg=None):
         return input_arg, other_arg
 
     input_arg, other_arg = parse_args(sys.argv[1:]) if arg is None else parse_args(arg)
-    print(input_arg)
+    print("input_arg", input_arg)
 
     if input_arg['SpeechMixEED']:
         model_type = "SpeechMixEED"
         if input_arg['lna']:
             model_type += "_lna"
+        elif input_arg['lnae']:
+            model_type += "_lnae"
         elif input_arg['fne']:
             model_type += "_fne"
         model = speechmix.SpeechMixEED(input_arg['speech_model_config'], input_arg['nlp_model_config'],
-                                       lna=input_arg['lna'], fne=input_arg['fne'])
+                                       lna=input_arg['lna'], lnae=input_arg['lnae'], fne=input_arg['fne'])
     elif input_arg['SpeechMixSelf']:
         model_type = "SpeechMixSelf"
         model = speechmix.SpeechMixSelf(input_arg['speech_model_config'], input_arg['nlp_model_config'])
@@ -109,8 +113,8 @@ def main(arg=None):
         model = speechmix.SpeechMixED(input_arg['speech_model_config'], input_arg['nlp_model_config'],
                                       ftl=input_arg['ftl'])
 
-    train_ds = load_dataset("common_voice", "zh-TW", split='train')
-    valid_ds = load_dataset("common_voice", "zh-TW", split='test')
+    train_ds = load_dataset(input_arg["dataset"], input_arg["field"], split=input_arg["split"])
+    valid_ds = load_dataset(input_arg["dataset"], input_arg["field"], split=input_arg["split"])
 
     train_ds = train_ds.cast_column("audio", Audio(sampling_rate=16_000))
     valid_ds = valid_ds.cast_column("audio", Audio(sampling_rate=16_000))
@@ -124,20 +128,20 @@ def main(arg=None):
         output_dir=f"./{input_arg['speech_model_config']}_{input_arg['nlp_model_config']}_{model_type}",
         per_device_train_batch_size=6,
         per_device_eval_batch_size=6,
-        gradient_accumulation_steps=10,
+        gradient_accumulation_steps=5,
         eval_accumulation_steps=2,
         evaluation_strategy="steps",
         group_by_length=True,
         num_train_epochs=60,
-        fp16=True,
+        fp16=False,
         save_steps=500,
         eval_steps=500,
-        logging_steps=500,
+        logging_steps=100,
         learning_rate=3e-4,
         warmup_steps=500,
         save_total_limit=2,
     )
-
+    # some trainer problem - save all logistics on compute_metrics, cause out of memory, fix:argmax first; dynamic padding on past key value, cause index error, fix: return only loss and logist
     trainer = Trainer(
         model=model,
         args=training_args,
@@ -145,7 +149,8 @@ def main(arg=None):
         train_dataset=train_ds,
         eval_dataset=valid_ds,
         data_collator=data_collator,
-        tokenizer=model.tokenizer
+        tokenizer=model.tokenizer,
+        callbacks=[EarlyStoppingCallback(early_stopping_patience=5)]
     )
 
     trainer.train()
--- speechmix/model.py
+++ speechmix/model.py
@@ -35,128 +35,8 @@ class SpeechMixED(nn.Module):
         return return_dict
 
 
-from torch import nn
-from transformers import Wav2Vec2Model, AutoModelForSeq2SeqLM, SpeechEncoderDecoderModel, AutoTokenizer, \
-    Wav2Vec2Processor, HubertModel, UniSpeechSatModel
-import torch
-
-
-def handle_decoder_input_none(decoder_config, device='cpu'):
-    return torch.tensor([[decoder_config.decoder_start_token_id]]).to(device)
-
-
-class SpeechMixED(nn.Module):
-    def __init__(self, speech_model_config, nlp_model_config, ftl=False):
-        super(SpeechMixED, self).__init__()
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.model = SpeechEncoderDecoderModel.from_encoder_decoder_pretrained(speech_model_config, nlp_model_config)
-        self.model.config.decoder_start_token_id = self.model.config.decoder.decoder_start_token_id
-        self.model.config.pad_token_id = self.model.config.decoder.pad_token_id
-        self.processor = Wav2Vec2Processor.from_pretrained(speech_model_config)
-        self.tokenizer = AutoTokenizer.from_pretrained(nlp_model_config)
-        if ftl:
-            for name, param in self.model.named_parameters():
-                if param.requires_grad:
-                    if any([k in name for k in ["encoder_attn", "enc_to_dec_proj", "embed_tokens"]]):
-                        param.requires_grad = True
-                    else:
-                        param.requires_grad = False
-
-    def forward(self, input_values, decoder_input_ids=None, labels=None):
-        if decoder_input_ids is None and labels is None:
-            decoder_input_ids = handle_decoder_input_none(self.model.config.decoder, self.device)
-        outputs = self.model(input_values=input_values, decoder_input_ids=decoder_input_ids, labels=labels)
-        return_dict = {'logits': torch.argmax(outputs['logits'], -1)}
-        if outputs.get('loss', None):
-            return_dict['loss'] = outputs['loss']
-        return return_dict
-
-
 class SpeechMixEED(nn.Module):
-    def __init__(self, speech_model_config, nlp_model_config, lna=False, fne=True):
-        super(SpeechMixEED, self).__init__()
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        if 'hubert' in speech_model_config:
-            self.encoder_model = HubertModel.from_pretrained(speech_model_config)
-        elif 'unispeech' in speech_model_config:
-            self.encoder_model = UniSpeechSatModel.from_pretrained(speech_model_config)
-        else:
-            self.encoder_model = Wav2Vec2Model.from_pretrained(speech_model_config).to(self.device)
-        self.decoder_model = AutoModelForSeq2SeqLM.from_pretrained(nlp_model_config).to(self.device)
-        self.processor = Wav2Vec2Processor.from_pretrained(speech_model_config)
-        self.tokenizer = AutoTokenizer.from_pretrained(nlp_model_config)
-        # remove last x layer
-        self.encoder_model.encoder.layers = self.encoder_model.encoder.layers[
-                                            :len(self.decoder_model.model.encoder.layers)]
-        self.enc_to_dec_proj = nn.Linear(self.encoder_model.config.hidden_size,
-                                         self.decoder_model.config.hidden_size).to(self.device)
-        self.length_adapter = nn.Conv1d(self.encoder_model.config.hidden_size, self.encoder_model.config.hidden_size,
-                                        2,
-                                        stride=2).to(self.device)
-        if lna:
-            for xcoder in [self.encoder_model.named_parameters, self.decoder_model.named_parameters]:
-                for name, param in xcoder():
-                    if param.requires_grad:
-                        if any([k in name for k in ["layer_norm", "encoder_attn"]]):
-                            param.requires_grad = True
-                        else:
-                            param.requires_grad = False
-        elif fne:
-            for name, param in self.decoder_model.named_parameters():
-                if param.requires_grad:
-                    param.requires_grad = False
-
-    def forward(self, input_values, decoder_input_ids=None, labels=None):
-        if decoder_input_ids is None and labels is None:
-            decoder_input_ids = handle_decoder_input_none(self.decoder_model.config, self.device)
-        inputs_embeds = self.encoder_model(input_values=input_values)['last_hidden_state']
-        for _ in range(3):
-            inputs_embeds = self.length_adapter(inputs_embeds.transpose(1, 2)).transpose(1, 2)
-        projected_embeds = self.enc_to_dec_proj(inputs_embeds)
-        outputs = self.decoder_model(inputs_embeds=projected_embeds,
-                                     decoder_input_ids=decoder_input_ids, labels=labels)
-        return_dict = {'logits': torch.argmax(outputs['logits'], -1)}
-        if outputs.get('loss', None):
-            return_dict['loss'] = outputs['loss']
-        return return_dict
-
-
-class SpeechMixSelf(SpeechMixEED):
-
-    def forward(self, input_values, input_ids=None, decoder_input_ids=None):
-        if decoder_input_ids is None:
-            decoder_input_ids = handle_decoder_input_none(self.decoder_model.config, self.device)
-        inputs_embeds = self.encoder_model(input_values=input_values.to(self.device))['last_hidden_state']
-        for _ in range(3):
-            inputs_embeds = self.length_adapter(inputs_embeds.transpose(1, 2)).transpose(1, 2)
-        projected_embeds = self.enc_to_dec_proj(inputs_embeds)
-
-        outputs = self.decoder_model(inputs_embeds=projected_embeds,
-                                     decoder_input_ids=decoder_input_ids)
-
-        if input_ids is not None:
-            nlp_hidden = \
-                self.decoder_model(input_ids.to(self.device), output_hidden_states=True)['encoder_hidden_states'][0]
-            attn_output = torch.bmm(nlp_hidden, projected_embeds.view(1, 768, -1))
-            voice_projected_embeds = torch.bmm(attn_output, projected_embeds)
-
-            kld_outputs = self.decoder_model(input_ids.to(self.device),
-                                             decoder_input_ids=decoder_input_ids)
-
-            mse_loss_fn = torch.nn.MSELoss()
-            kld_loss_fn = torch.nn.KLDivLoss()
-
-            loss = kld_loss_fn(kld_outputs.logits, outputs.logits)
-            loss += mse_loss_fn(voice_projected_embeds, nlp_hidden)
-
-        return_dict = {'logits': torch.argmax(outputs['logits'], -1)}
-        if outputs.get('loss', None):
-            return_dict['loss'] = loss
-        return return_dict
-
-
-class SpeechMixEED(nn.Module):
-    def __init__(self, speech_model_config, nlp_model_config, lna=False, fne=True):
+    def __init__(self, speech_model_config, nlp_model_config, lnae=False, lna=False, fne=True):
         super(SpeechMixEED, self).__init__()
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         if 'hubert' in speech_model_config:
@@ -176,14 +56,14 @@ class SpeechMixEED(nn.Module):
         self.length_adapter = nn.Conv1d(self.encoder_model.config.hidden_size, self.encoder_model.config.hidden_size,
                                         2,
                                         stride=2).to(self.device)
-        if lna:
+        if lna or lnae:
             for xcoder in [self.encoder_model.named_parameters, self.decoder_model.named_parameters]:
                 for name, param in xcoder():
                     if param.requires_grad:
                         if any([k in name for k in
-                                ["layer_norm", "encoder_attn", 'attention', 'enc_to_dec_proj', 'length_adapter',
-                                 'lm_head', 'shared']]) or (
-                                'encoder' in name and 'self_attn' in name):
+                                ["layer_norm", "encoder_attn", 'enc_to_dec_proj', 'length_adapter', 'lm_head',
+                                 'shared']]) or (
+                                lnae and ('attention' in name or ('encoder' in name and 'self_attn' in name))):
                             param.requires_grad = True
                         else:
                             param.requires_grad = False
@@ -216,7 +96,6 @@ class SpeechMixSelf(SpeechMixEED):
         for _ in range(3):
             inputs_embeds = self.length_adapter(inputs_embeds.transpose(1, 2)).transpose(1, 2)
         projected_embeds = self.enc_to_dec_proj(inputs_embeds)
-
         outputs = self.decoder_model(inputs_embeds=projected_embeds,
                                      decoder_input_ids=decoder_input_ids)
         loss = None

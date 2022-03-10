d697d9bcc8fdd20c4bab0b6022f44464c6716b26
voidful <voidful.stack@gmail.com>
2022-03-11 03:13:57 +0800

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='speechmix',
-    version='0.0.29',
+    version='0.0.30',
     description='Explore different way to mix speech model(wav2vec2, hubert) and nlp model(BART,T5,GPT) together',
     url='https://github.com/voidful/SpeechMix',
     author='Voidful',
